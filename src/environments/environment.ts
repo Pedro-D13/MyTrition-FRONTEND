db77f5506a418a92275258fd5c5755c5a09138cb
Pedro Dias <p.afonsosd1994@gmail.com>
2020-04-14 21:52:28 +0100

    Removal of firebase API KEY

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,20 +2,6 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyDI6zPbGEp3NWEgcBZAQ5nnabqbSZ8zKT4",
-    authDomain: "portfolio-b0893.firebaseapp.com",
-    databaseURL: "https://portfolio-b0893.firebaseio.com",
-    projectId: "portfolio-b0893",
-    storageBucket: "portfolio-b0893.appspot.com",
-    messagingSenderId: "52653017588",
-    appId: "1:52653017588:web:fccc8a7b7c3ef55a6851a1",
-    measurementId: "G-8PHM57Z2L0"
-  }
-};
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
